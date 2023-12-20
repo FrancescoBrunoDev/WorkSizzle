fbff483d982895192c515f3df1496eb292e3f1aa
Francesco Bruno <59353843+FrancescoBrunoDev@users.noreply.github.com>
2023-12-20 11:20:32 +0000

    Add calendar component and format date

--- components/ui/calendar/mainView.tsx
+++ components/ui/calendar/mainView.tsx
@@ -5,6 +5,7 @@ import Controls from "@/components/ui/calendar/controls";
 import { Separator } from "@/components/ui/separator";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import Link from "next/link";
+import { Calendar } from "@/components/ui/calendar";
 
 interface holiday {
   date: string;
@@ -16,6 +17,21 @@ interface holiday {
   counties: string[] | null;
   launchYear: number | null;
   type: string;
+}
+
+// https://stackoverflow.com/a/23593099
+function formatDate(date: Date) {
+  var d = new Date(date),
+    month = '' + (d.getMonth() + 1),
+    day = '' + d.getDate(),
+    year = d.getFullYear();
+
+  if (month.length < 2) 
+    month = '0' + month;
+  if (day.length < 2) 
+    day = '0' + day;
+
+  return [year, month, day].join('-');
 }
 
 export default function MainView({
@@ -43,7 +59,7 @@ export default function MainView({
       if (
         date.getDay() !== 0 &&
         date.getDay() !== 6 &&
-        !holidaysForYear.includes(date.toISOString().slice(0, 10))
+        !holidaysForYear.includes(formatDate(date))
       ) {
         count++;
       }
@@ -55,24 +71,32 @@ export default function MainView({
     return { total: count, twentyPercent };
   }
 
+
   const workingDaysInMonths = Array.from({ length: 12 }, (_, i) =>
     calculateTwentyPercentDays(i, year)
   );
+
+  function isDisabled(date: Date) {
+    if (date.getDay() === 0 || date.getDay() === 6 || holidaysForYear.includes(date.toISOString().slice(0, 10))) {
+      return true;
+    }
+    return false;
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between gap-4 container py-20">
       <p className="font-medium">Days designated for office work</p>
       <div className="flex items-center gap-6 h-36">
         <Link href={`/${Number(year) - 1}`}>
-            <button className="p-2 bg-secondary rounded-lg">
-                <ChevronLeft strokeWidth={2.25} />
-            </button>
+          <button className="p-2 bg-secondary rounded-lg">
+            <ChevronLeft strokeWidth={2.25} />
+          </button>
         </Link>
         <h1 className="font-black text-8xl">{year}</h1>
         <Link href={`/${Number(year) + 1}`}>
-            <button className="p-2 bg-secondary rounded-lg">
-                <ChevronRight strokeWidth={2.25} />
-            </button>
+          <button className="p-2 bg-secondary rounded-lg">
+            <ChevronRight strokeWidth={2.25} />
+          </button>
         </Link>
       </div>
       <Separator />
@@ -118,6 +142,16 @@ export default function MainView({
                   </ul>
                 </div>
               )}
+              <div className="w-full flex justify-center">
+                <Calendar
+                  mode="multiple"
+                  defaultMonth={new Date(year, index)}
+                  disabled={isDisabled}
+                  ISOWeek={true}
+                  disableNavigation={true}
+                  hideHead={false}
+                />
+              </div>
             </div>
           );
         })}
