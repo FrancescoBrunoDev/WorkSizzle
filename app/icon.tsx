dd9cf838274f7050dc932893b707364a2973bf52
franzbruno95 <59353843+FrancescoBrunoDev@users.noreply.github.com>
2024-04-24 11:32:03 +0000

    added favicon

---
+++ app/icon.tsx
@@ -0,0 +1,38 @@
+import { ImageResponse } from 'next/og'
+
+// Image metadata
+export const size = {
+    width: 32,
+    height: 32,
+}
+export const contentType = 'image/png'
+
+// Image generation
+export default function Icon() {
+    return new ImageResponse(
+        (
+            // ImageResponse JSX element
+            <div
+                style={{
+                    fontSize: 16,
+                    fontWeight: 'bold',
+                    background: 'rgb(242 252 247)',
+                    width: '100%',
+                    height: '100%',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    color: 'rgb(51 207 128)',
+                }}
+            >
+                WS
+            </div>
+        ),
+        // ImageResponse options
+        {
+            // For convenience, we can re-use the exported icons size metadata
+            // config to also set the ImageResponse's width and height.
+            ...size,
+        }
+    )
+}
