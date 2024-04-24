9757931a08f85308f4cba5875685bf3d2af0de4a
franzbruno95 <59353843+FrancescoBrunoDev@users.noreply.github.com>
2024-04-24 22:04:56 +0200

    Added Toaster

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/styles/globals.css";
 import Navbar from "@/components/ui/navbar";
+import { Toaster } from "@/components/ui/sonner"
 
 const inter = Inter({
   subsets: ["latin"],
@@ -24,6 +25,7 @@ export default function RootLayout({
       <body className="min-h-screen bg-background font-sans antialiased">
         <Navbar />
         {children}
+        <Toaster position="bottom-center" expand={true} richColors />
       </body>
     </html>
   );
